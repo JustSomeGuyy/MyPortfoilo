9912608023f7ffd127f2338fc6c0125da33b1dd2
Tyler Wojciechowski <106572294+JustSomeGuyy@users.noreply.github.com>
2025-03-17 14:49:07 -0500

    Updated the re-design of the weather app along with updating the description of the weather app expanding on some features.

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -8,10 +8,10 @@ let projects = [
 		languages: ['Svelte', 'SvelteKit', 'TailwindCSS', 'TypeScript']
 	},
 	{
-		name: 'ClearSky',
-		source: '/assets/project_photos/ClearSky.png',
+		name: 'Weather App',
+		source: '/assets/project_photos/WeatherApp.png',
 		link: 'https://justsomeguyy.github.io/TOP-Weather-App/',
-		description: 'This is a project I created while learning full-stack JavaScript through The Odin Project. I worked alongside APIs, reading and displaying .json files, and working with system functions.',
+		description: 'This is a project I created while learning full-stack JavaScript through The Odin Project. I worked alongside APIs, reading and displaying .json files, and working with system functions. This app will auto-generate a location if the user blocks the location request. All temperatures are displayed in Celsius.',
 		languages: ['HTML5', 'SASS', 'JavaScript']
 	}
 ];
